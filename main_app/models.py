183ceedf309b2c1cf08576dd6df187cc2bec6421
joshuabuster <63068851+joshuabuster@users.noreply.github.com>
2020-09-19 11:30:00 -0700

    auth and recipe and meal models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import date
 
 # Create your models here.
+
+
+FAVORITES = (
+    ('N', 'Not'),
+    ('F', 'Favorite')
+)
+
+MAIN_INGREDIENT = (
+    ('C', 'Chicken'),
+    ('P', 'Pork'),
+    ('B', 'Beef'),
+    ('F', 'Fish'),
+    ('S', 'Shellfish'),
+    ('L', 'Lamb'),
+    ('N', 'Pasta'),
+    ('V', 'Vegetarian'),
+)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    source = models.CharField(max_length=100)
+    instructions = models.TextField()
+    favorite = models.CharField(
+        max_length=1,
+        choices=FAVORITES,
+        default=FAVORITES[0][0]
+    )
+    main_ingredient = models.CharField(
+        max_length=1,
+        choices=MAIN_INGREDIENT,
+        default=MAIN_INGREDIENT[0][0]
+    )    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'meal_id': self.id})
+
+class Meal(models.Model):
+    date = models.DateField('meal date')
+    recipe = models.ManyToManyField(Recipe)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'meal_id': self.id})
+
+
