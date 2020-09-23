ed3934ec218216e2d15f7388a06932e04b63ff89
joshuabuster <63068851+joshuabuster@users.noreply.github.com>
2020-09-23 12:43:11 -0700

    deletes some dead code

--- main_app/views.py
+++ main_app/views.py
@@ -104,18 +104,13 @@ class MainList(LoginRequiredMixin, ListView):
     pass
 
 def add_photo(request, recipe_id):
-    # photo-file will be the "name" attribute on the <input type="file">
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
         s3 = boto3.client('s3')
-        # need a unique "key" for S3 / needs image file extension too
         key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-        # just in case something goes wrong
         try:
             s3.upload_fileobj(photo_file, BUCKET, key)
-            # build the full url string
             url = f"{S3_BASE_URL}{BUCKET}/{key}"
-            # we can assign to cat_id or cat (if you have a cat object)
             Photo.objects.create(url=url, recipe_id=recipe_id)
         except:
             print('An error occurred uploading file to S3')
